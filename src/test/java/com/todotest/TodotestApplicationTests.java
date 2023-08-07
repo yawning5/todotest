85a4939b933406ee2604039edd24f690d0160864
yawn <yawning055@gmail.com>
2023-08-07 13:51:51 +0900

    homework

---
+++ src/test/java/com/todotest/TodotestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.todotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
